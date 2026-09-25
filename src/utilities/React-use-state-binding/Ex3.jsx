d9e87cd91e85e777085b985830c44c3fba1eae77
amanieltewolde <aman94@hotmail.it>
2026-09-25 23:58:36 +0200

    Ex-3R.Use-state

--- src/utilities/React-use-state-binding/Ex3.jsx
+++ src/utilities/React-use-state-binding/Ex3.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
 import { exList } from "./List-Exercises";
 
 export default function Ex3() {
+    const [textAlign, setTextAlign] = useState('text-start');
+
+
     return (
         <div>
             <h6>{exList[2].text}</h6>
+            <div className="container bg-secondary p-5 m-5 d-flex gap-3 justify-content-center ">
+                <button onClick={() => setTextAlign('text-start')} className="btn btn-warning">Testo a sinistra</button>
+                <button onClick={() => setTextAlign('text-center')} className="btn btn-dark">Testo centrale</button>
+                <button onClick={() => setTextAlign('text-end')} className="btn btn-danger">Testo a destra</button>
+            </div>
+            <p className={`fs-6 mark text-uppercase ${textAlign}`}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         </div>
     )
 }
